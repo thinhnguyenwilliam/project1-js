40301db4e9ed14e0fd3d5eccfc8928fea6463baf
ThinhNguyen <thinhproee@gmail.com>
2024-12-19 23:51:51 +0700

    init  project

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/products', userRoutes);
+
+// MongoDB Connection
+// mongoose.connect('mongodb://localhost:27017/express-mvc', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch(err => console.error('Could not connect to MongoDB:', err));
+
+// Start Server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ src/services/userService.js
@@ -0,0 +1,26 @@
+const User = require('../models/userModel');
+
+class UserService {
+  async createUser(userData) {
+    return await User.create(userData);
+  }
+
+  async getAllUsers() 
+  {
+    return await User.find();
+  }
+
+  async getUserById(userId) {
+    return await User.findById(userId);
+  }
+
+  async updateUser(userId, updatedData) {
+    return await User.findByIdAndUpdate(userId, updatedData, { new: true });
+  }
+
+  async deleteUser(userId) {
+    return await User.findByIdAndDelete(userId);
+  }
+}
+
+module.exports = new UserService();
